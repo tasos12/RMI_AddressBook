b39c96f5c744801a24087e325085dda534c78108
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2019-03-24 21:13:29 +0200

    Added Documentation

--- srcServer/AddressBook.java
+++ srcServer/AddressBook.java
@@ -3,9 +3,40 @@ import java.rmi.*;
 
 public interface AddressBook extends Remote{
 	
+	/**
+	 * Inserts the address book entry specified by the client to the database
+	 * @param entry the BookEntry to be inserted.
+	 * @return  	the number of lines changed in database
+	 * @see			BookEntry
+	 */
 	String insert(BookEntry entry) throws RemoteException;
+
+	/**
+	 * Updates the BookEntry specified by the id with the new BookEntry
+	 * @param id 	the id of BookEntry to update
+	 * @param entry the BookEntry to replace the old one
+	 * @return 		the number of lines updated
+	 * @see			BookEntry
+	 */
 	String update(int id, BookEntry entry) throws RemoteException;
+
+	/**
+	 * Selects the BookEntry to return to the client
+	 * @param id	the id of BookEntry to be returned
+	 * @return		the selected Bookentry as string
+	 */
 	String select(int id) throws RemoteException;
+
+	/**
+	 * Deletes the selected BookEntry from database
+	 * @param id	the id of
+	 * @return 		a text with the number of lines deleted 
+	 */
 	String delete(int id) throws RemoteException;
+
+	/**
+	 * Selects and returns all database Book Entries as text
+	 * @return		a text with all the registered BookEntries
+	 */
 	String selectAll() throws RemoteException;
 }
--- srcServer/AddressBookImpl.java
+++ srcServer/AddressBookImpl.java
@@ -2,6 +2,14 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.*;
 
+
+/**
+ * The Address Book implementation implements the AddressBook class and provides the server with 
+ * its basic functionalities
+ * 
+ * @author Gkagkas Anastasios
+ * @since 2019-03
+ */
 public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 
 	private static final long serialVersionUID = 8197529583444611357L;
@@ -13,6 +21,13 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		statement = s;
 	}
 	
+
+	/**
+	 * Inserts the address book entry specified by the client to the database
+	 * @param entry the BookEntry to be inserted.
+	 * @return  	the number of lines changed in database
+	 * @see			BookEntry
+	 */
 	public String insert(BookEntry entry) throws RemoteException{
 		String text = "Error while inserting";
 		String query = "insert into users values (" + entry.getId() + ", \"" + entry.getFullname()
@@ -27,6 +42,14 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		return text;
 	}
 
+
+	/**
+	 * Updates the BookEntry specified by the id with the new BookEntry
+	 * @param id 	the id of BookEntry to update
+	 * @param entry the BookEntry to replace the old one
+	 * @return 		the number of lines updated
+	 * @see			BookEntry
+	 */
 	public String update(int id, BookEntry entry) throws RemoteException{
 		String text = "Error while updating";
 		String query = "update users set id = " + entry.getId() + ", fullname = \"" + entry.getFullname()
@@ -42,6 +65,11 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		return text;
 	}
 
+	/**
+	 * Selects the BookEntry to return to the client
+	 * @param id	the id of BookEntry to be returned
+	 * @return		the selected Bookentry as string
+	 */
 	public String select(int id) throws RemoteException{
 		String text = "Error while selecting";
 		String query = "select * from users where id = " + id;
@@ -55,6 +83,11 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		return text;
 	}
 
+	/**
+	 * Deletes the selected BookEntry from database
+	 * @param id	the id of
+	 * @return 		a text with the number of lines deleted 
+	 */
 	public String delete(int id) throws RemoteException{
 		String text = "Error while deleting";
 		String query = "delete from users where id = " + id;
@@ -68,6 +101,10 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		return text;
 	}
 
+	/**
+	 * Selects and returns all database Book Entries as text
+	 * @return		a text with all the registered BookEntries
+	 */
 	public String selectAll() throws RemoteException{
 		String text = "";
 		String query = "select * from users";
@@ -81,19 +118,29 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook{
 		return text;
 	}
 
-	private String result2String(ResultSet r) throws Exception{
+	/**
+	 * Transforms the result of the query to string
+	 * @param result	the ResultSet of the query result
+	 * @return			a text from the ResultSet
+	 */
+	private String result2String(ResultSet result) throws Exception{
 		String text = "";
-		while(r.next()){
-			int tempId = r.getInt("id");
-			String tempFn = r.getString("fullname");
-			String tempMail = r.getString("email");
-			int tempPhone = r.getInt("phone");
+		while(result.next()){
+			int tempId = result.getInt("id");
+			String tempFn = result.getString("fullname");
+			String tempMail = result.getString("email");
+			int tempPhone = result.getInt("phone");
 			BookEntry temp = new BookEntry(tempId, tempFn, tempMail, tempPhone);
 			text += temp.toString();
 		}
 		return text;
 	}
 	
+	/**
+	 * Transforms the integer result of the query to string
+	 * @param result	an integer as a result of query
+	 * @return			a text based on the result
+	 */
 	private String updateResult2String(int result) throws Exception{
 		String text = "Number of lines updated: " + result;
 		return text;
